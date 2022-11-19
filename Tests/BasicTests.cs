1120ddbd93cef217d52874a4ab1fc933e88af716
chris rogers <tochrisrogers@gmail.com>
2022-11-19 14:08:21 -0800

    Added Tests folder to hold test classes. Added BasicTests.cs to Tests folder to store basic test methods.

---
+++ Tests/BasicTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Windows;
+using System.Diagnostics;
+using System.ComponentModel;
+using AppiumTest.UIActions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppiumTest.AppControls;
+
+namespace AppiumTest.Tests
+{
+    internal class BasicTests
+    {
+        [ClassInitialize]
+        public void ClassInit()
+        {
+            new AppiumService()
+                .StopAppium();
+        }
+
+        [ClassCleanup]
+        public void ClassCleanup()
+        {
+            new AppiumService()
+                .StopAppium();
+        }
+
+        [TestMethod]
+        public void LaunchBrowser()
+        {
+            new AppiumService()
+                .StartAppium()
+                .LaunchApp(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe");
+        }
+
+
+
+    }
+}
